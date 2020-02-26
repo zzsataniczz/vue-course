fb917eef42df069f1a0ce37cd1fb7d7d4310bf45
Vadim Dymov <Vadim.I.Dymov@tatar.ru>
2020-02-27 01:37:10 +0300

    Использовал другок апи, изменил структуру проекта, изменил темплэйт

--- stubs/index.js
+++ stubs/index.js
@@ -4,10 +4,19 @@ const axios = require('axios').default;
 
 app.use(express.static("dist"));
 
-app.get(["/api"], function (request, response) {
-    axios.get('http://data.fixer.io/api/latest?access_key=543c7963c89af43c44fc680b2c3a3ab0')
+app.get(["/getDataByBaseQuote"], function (request, response) {
+    let quotes = ['BCHEUR,BCHGBP,BCHXRP'];
+    if (typeof request.query.selectedQuotes !== 'undefined' && request.query.selectedQuotes.length > 0) {
+        quotes = request.query.selectedQuotes;
+    }
+    axios.get('https://currate.ru/api/?get=rates&pairs='+ quotes.join(',') +'&key=8c33f32ce2ae72696d3cd1669b68d069')
         .then(responseFromApi => response.send(responseFromApi.data))
         .catch(error => response.send(error))
+});
+app.get(["/getQuotes"], function (request, response) {
+    axios.get('https://currate.ru/api/?get=currency_list&key=8c33f32ce2ae72696d3cd1669b68d069')
+        .then(responseFromApi => response.send(responseFromApi.data))
+        .catch(error => response.send(error));
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
