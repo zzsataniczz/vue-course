f1810aa8127743918c54f67e7c50d0b7070dcdaf
Vadim Dymov <Vadim.I.Dymov@tatar.ru>
2020-02-28 22:01:40 +0300

    Произвел небольшой рефакторинг

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,10 +6,17 @@
             </md-card-header>
 
             <md-card-content>
-                1. Отображение информации по различным валютным котировкам (табличное представление)
-                2. Возможность дополнительно выбрать отображаемые котировки
-
-                Данные берутся с внешнего бесплатного апи: https://currate.ru/
+                <md-list>
+                    <md-list-item>
+                        <span class="md-list-item-text">1. Отображение информации по различным валютным котировкам (табличное представление)</span>
+                    </md-list-item>
+                    <md-list-item>
+                        <span class="md-list-item-text">2. Возможность дополнительно выбрать отображаемые котировки</span>
+                    </md-list-item>
+                    <md-list-item>
+                        <span class="md-list-item-text">3. Данные берутся с внешнего бесплатного апи: https://currate.ru</span>
+                    </md-list-item>
+                </md-list>
             </md-card-content>
         </md-card>
     </div>
