11ec9a995d6dfc99e7d84cf700d0a6013e271252
Vadim Dymov <Vadim.I.Dymov@tatar.ru>
2020-02-29 23:52:00 +0300

    Вынес константы, мелкие правки

---
+++ src/__data__/constants/default_quotes.js
@@ -0,0 +1,5 @@
+let DEF_DATA = {
+    QUOTE_PAIRS: ['BCHEUR,BCHGBP,BCHXRP']
+};
+
+module.exports = {DEF_DATA};
--- stubs/index.js
+++ stubs/index.js
@@ -11,23 +11,26 @@ app.use(staticFileMiddleware);
 app.use(history());
 app.use(staticFileMiddleware);
 
+const {API_URLS} = require('../src/__data__/constants/api-urls.js');
+const {API_KEYS} = require('../src/__data__/constants/api-keys.js');
+const {DEF_DATA} = require('../src/__data__/constants/default_quotes');
+
 app.get('/', function (req, res) {
     res.render(path.join(__dirname + '/index.html'))
 });
 
 app.get(["/getDataByBaseQuote"], function (request, response) {
-    let quotes = ['BCHEUR,BCHGBP,BCHXRP'];
-    console.log(request.query);
-    console.log(request.params);
+    let quotes = DEF_DATA.QUOTE_PAIRS;
     if (typeof request.query.selectedQuotes !== 'undefined' && request.query.selectedQuotes.length > 0) {
         quotes = request.query.selectedQuotes;
     }
-    axios.get('https://currate.ru/api/?get=rates&pairs='+ quotes.join(',') +'&key=8c33f32ce2ae72696d3cd1669b68d069')
+    axios.get(API_URLS.CURRATE + '?get=rates&pairs=' + quotes.join(',') + '&key=' + API_KEYS.CURRATE)
         .then(responseFromApi => response.send(responseFromApi.data))
         .catch(error => response.send(error))
 });
+
 app.get(["/getQuotes"], function (request, response) {
-    axios.get('https://currate.ru/api/?get=currency_list&key=8c33f32ce2ae72696d3cd1669b68d069')
+    axios.get(API_URLS.CURRATE + '?get=currency_list&key=' + API_KEYS.CURRATE)
         .then(responseFromApi => response.send(responseFromApi.data))
         .catch(error => response.send(error));
 });
