38907c0321fcf3876da47da204f6eda601da5405
Vadim Dymov <Vadim.I.Dymov@tatar.ru>
2020-03-01 14:49:07 +0300

    Добавил валидацию на новое поле

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import store from './__data__/store'
 import router from './router'
+import VeeValidate from 'vee-validate';
 
 Vue.use(VueMaterial)
+Vue.use(VeeValidate)
 
 new Vue({
     store,
